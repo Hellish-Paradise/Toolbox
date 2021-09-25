b26be33dc3c4cd35ad155f0cec63dc6a4102442b
01WORLD <00onezero01@gmail.com>
2021-09-25 20:44:42 +0800

    添加详细的help命令帮助

--- src/com/sin/tool/view/UI.java
+++ src/com/sin/tool/view/UI.java
@@ -62,6 +62,26 @@ public class UI {
                     case "help":
                         System.out.format("%-30s","exit");
                         System.out.format("%s\n", "退出Toolbox");
+                        System.out.format("%-30s","hashsafe");
+                        System.out.format("%s\n","进行文件内容Hash加密");
+                        System.out.format("%-30s","key -c");
+                        System.out.format("%s\n","创建公钥与私钥");
+                        System.out.format("%-30s","key -s");
+                        System.out.format("%s\n","使用私钥对文件内容进行签名");
+                        System.out.format("%-30s","key -p");
+                        System.out.format("%s\n","使用公钥对文件内容进行签名验证");
+                        System.out.format("%-30s","pwd");
+                        System.out.format("%s\n","随机密码生成");
+                        System.out.format("%-30s","aessafe -e");
+                        System.out.format("%s\n","进行AES加密");
+                        System.out.format("%-30s","aessafe -d");
+                        System.out.format("%s\n","进行AES解密");
+                        System.out.format("%-30s","logs -s");
+                        System.out.format("%s\n","查看全部日志");
+                        System.out.format("%-30s","logs -sa");
+                        System.out.format("%s\n","查看指定日期日志");
+                        System.out.format("%-30s","logs -d");
+                        System.out.format("%s\n","删除日志");
                         break;
                     default:
                         System.out.println("请输入正确指令");
